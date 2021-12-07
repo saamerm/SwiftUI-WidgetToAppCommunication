7c1b914b1ae7c8f7c78a4d8ab7bbfb3d0ae254a1
Saamer Mansoor <i@saamer.me>
2021-12-07 16:52:45 -0500

    Blank Template with Widget working

---
+++ StairClimber/StairClimber/StairClimberApp.swift
@@ -0,0 +1,17 @@
+//
+//  StairClimberApp.swift
+//  StairClimber
+//
+//  Created by Saamer Mansoor on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct StairClimberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
