44b90d5a0754367210dfeec8187654a59ff337a1
Saamer Mansoor <i@saamer.me>
2021-12-09 02:18:47 -0500

    Simple Widget Update

--- StairClimber/StairClimber/ContentView.swift
+++ StairClimber/StairClimber/ContentView.swift
@@ -1,17 +1,13 @@
 import Foundation
 import SwiftUI
 struct ContentView: View {
-    @State private var joke: String = ""
     var body: some View {
-        Text(joke)
         Button {
             Task {
-                let (data, _) = try await URLSession.shared.data(from: URL(string:"https://api.chucknorris.io/jokes/random")!)
-                let decodedResponse = try? JSONDecoder().decode(Joke.self, from: data)
-                joke = decodedResponse?.value ?? ""
+                WidgetCenter.shared.reloadAllTimelines()
             }
         } label: {
-            Text("Fetch Joke")
+            Text("Update Widget")
         }
     }
 }
@@ -20,8 +16,4 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
-
-struct Joke: Codable {
-    let value: String
 }
